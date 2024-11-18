b95fb465ddc59401819e280c236152a24b1084af
Fabio Pereira <fabiuhp@msn.com>
2024-11-18 08:03:48 -0300

    feat: integracao com redis

--- app/app.go
+++ app/app.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	return &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 }
 
@@ -22,9 +26,16 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("error starting server: %w", err)
+		return fmt.Errorf("erro ao conectar o redis: %w", err)
+	}
+
+	fmt.Println("Iniciando server")
+
+	err = server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("erro iniciando server: %w", err)
 	}
 
 	return nil
